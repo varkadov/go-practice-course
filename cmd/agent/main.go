81189faa77e22dc3af9ea5fc878635979b8d8387
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-07 22:57:36 +0300

    Agent draft

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,65 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+const (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+const url = "http://localhost:8080/update/gauge/10/10"
+
+func main() {
+	c := http.Client{}
+	m := runtime.MemStats{}
+
+	pollChan := make(chan interface{})
+	reportChan := make(chan interface{})
+
+	go pollFn(pollChan)
+	go reportFn(reportChan)
+
+	for {
+		select {
+		case <-pollChan:
+			{
+				fmt.Println("It's time for report")
+
+				runtime.ReadMemStats(&m)
+
+				fmt.Printf("Alloc: %v\n", m.Alloc)
+			}
+		case <-reportChan:
+			{
+				fmt.Println("It's time for poll")
+
+				res, err := c.Post(url, "text/plain", nil)
+				if err != nil {
+					_ = fmt.Errorf("%v", err)
+					return
+				}
+
+				fmt.Printf("Status code: %d\n", res.StatusCode)
+			}
+		}
+	}
+}
+
+func pollFn(ch chan interface{}) {
+	for {
+		time.Sleep(pollInterval)
+		ch <- nil
+	}
+}
+
+func reportFn(ch chan interface{}) {
+	for {
+		time.Sleep(reportInterval)
+		ch <- nil
+	}
+}
