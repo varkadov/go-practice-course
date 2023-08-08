1d76147e16506b74fc7a66f7ef41b5b545eaffcf
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-08 22:34:25 +0300

    [Server] Add a flag

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/varkadov/go-practice-course/cmd/internal/handlers"
 	"github.com/varkadov/go-practice-course/cmd/internal/storage"
@@ -12,11 +14,15 @@ func main() {
 	s := storage.NewMemStorage()
 	r := chi.NewRouter()
 
+	addr := flag.String("a", ":8080", "Server address")
+	flag.Parse()
+
 	r.Get("/", handlers.RootHandler(s))
 	r.Get("/value/{metricType}/{metricName}", handlers.GetMetricHandler(s))
 	r.Post("/update/{metricType}/{metricName}/{metricValue}", handlers.PostMetricHandler(s))
 
-	err := http.ListenAndServe(":8080", r)
+	fmt.Printf("Server running on %s", *addr)
+	err := http.ListenAndServe(*addr, r)
 	if err != nil {
 		log.Fatal(err)
 	}
