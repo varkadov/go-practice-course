2b147169b5897a3b716652cb90fb91ccb650ca0b
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-08 17:21:26 +0300

    [Server] Add storage.Get method

--- cmd/internal/handlers/GetMetricHandler.go
+++ cmd/internal/handlers/GetMetricHandler.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/varkadov/go-practice-course/cmd/internal/storage"
 	"io"
@@ -14,40 +13,18 @@ func GetMetricHandler(s *storage.MemStorage) func(w http.ResponseWriter, r *http
 		metricType := strings.ToLower(chi.URLParam(r, "metricType"))
 		metricName := strings.ToLower(chi.URLParam(r, "metricName"))
 
-		if metricType == "gauge" {
-			if v, ok := s.Gauge[metricName]; ok {
-				s := fmt.Sprintf("%f", v)
-
-				_, err := io.WriteString(w, s)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
-			http.Error(w, "", http.StatusNotFound)
-			return
-		} else if metricType == "counter" {
-			if v, ok := s.Counter[metricName]; ok {
-				s := fmt.Sprintf("%d", v)
-
-				_, err := io.WriteString(w, s)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
-			http.Error(w, "", http.StatusNotFound)
+		v, err := s.Get(metricType, metricName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
 
-		w.WriteHeader(http.StatusBadRequest)
+		_, err = io.WriteString(w, v)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
--- cmd/internal/storage/MemStorage.go
+++ cmd/internal/storage/MemStorage.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"errors"
+	"fmt"
+)
+
 type MemStorage struct {
 	Gauge   map[string]float64
 	Counter map[string]int64
@@ -10,4 +15,22 @@ func NewMemStorage() *MemStorage {
 		Gauge:   make(map[string]float64),
 		Counter: make(map[string]int64),
 	}
+}
+
+func (s *MemStorage) Get(t, n string) (string, error) {
+	if t == "gauge" {
+		if v, ok := s.Gauge[n]; ok {
+			return fmt.Sprintf("%f", v), nil
+		}
+		return "", errors.New("metric doesn't exist")
+	}
+
+	if t == "counter" {
+		if v, ok := s.Counter[n]; ok {
+			return fmt.Sprintf("%d", v), nil
+		}
+		return "", errors.New("metric doesn't exist")
+	}
+
+	return "", errors.New("metric doesn't exist")
 }
