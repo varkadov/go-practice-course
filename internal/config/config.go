b711306e4935ba20112de040bf158a57d7cd8468
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-12 11:19:08 +0300

    [Review] Use pointers for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,35 +8,35 @@ import (
 )
 
 type Config struct {
-	Addr           string `env:"ADDRESS"`
-	PollInterval   int    `env:"POLL_INTERVAL"`
-	ReportInterval int    `env:"REPORT_INTERVAL"`
+	Addr           *string `env:"ADDRESS"`
+	PollInterval   *int    `env:"POLL_INTERVAL"`
+	ReportInterval *int    `env:"REPORT_INTERVAL"`
 }
 
 func NewConfig() *Config {
 	var (
 		config         Config
-		addr           string
-		pollInterval   int
-		reportInterval int
+		addr           *string
+		pollInterval   *int
+		reportInterval *int
 	)
 
 	if err := env.Parse(&config); err != nil {
 		log.Fatal(err)
 	}
 
-	flag.StringVar(&addr, "a", "localhost:8080", "Server address")
-	flag.IntVar(&pollInterval, "p", 2, "Pool Interval")
-	flag.IntVar(&reportInterval, "r", 10, "Report interval")
+	flag.StringVar(addr, "a", "localhost:8080", "Server address")
+	flag.IntVar(pollInterval, "p", 2, "Pool Interval")
+	flag.IntVar(reportInterval, "r", 10, "Report interval")
 	flag.Parse()
 
-	if config.Addr == "" {
+	if config.Addr == nil {
 		config.Addr = addr
 	}
-	if config.PollInterval == 0 {
+	if config.PollInterval == nil {
 		config.PollInterval = pollInterval
 	}
-	if config.ReportInterval == 0 {
+	if config.ReportInterval == nil {
 		config.ReportInterval = reportInterval
 	}
 
