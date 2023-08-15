6cae0ad3d0eab58f9d02b99ad3ac2452c886cd09
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-15 21:27:55 +0300

    [Review] Handle all metrics

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"runtime"
 	"time"
@@ -9,12 +10,12 @@ import (
 	"github.com/varkadov/go-practice-course/internal/config"
 )
 
-const path = "/update/gauge/10/10"
-
 func main() {
 	c := http.Client{}
-	m := runtime.MemStats{}
 	conf := config.NewConfig()
+
+	m := runtime.MemStats{}
+	pollCount := 0
 
 	pollTimer := time.NewTicker(time.Duration(conf.PollInterval) * time.Second)
 	reportTimer := time.NewTicker(time.Duration(conf.ReportInterval) * time.Second)
@@ -22,19 +23,63 @@ func main() {
 	defer reportTimer.Stop()
 	defer pollTimer.Stop()
 
-	url := "http://" + conf.Addr + path
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	host := "http://" + conf.Addr
 
 	for {
 		select {
 		case <-pollTimer.C:
-			runtime.ReadMemStats(&m)
+			pollCount++
 		case <-reportTimer.C:
-			res, err := c.Post(url, "text/plain", nil)
-			if err != nil {
-				_ = fmt.Errorf("%v", err)
-				return
+			runtime.ReadMemStats(&m)
+
+			paths := []string{
+				fmt.Sprintf("/update/gauge/Alloc/%d", m.Alloc),
+				fmt.Sprintf("/update/gauge/BuckHashSys/%d", m.BuckHashSys),
+				fmt.Sprintf("/update/gauge/Frees/%d", m.Frees),
+				fmt.Sprintf("/update/gauge/GCCPUFraction/%f", m.GCCPUFraction),
+				fmt.Sprintf("/update/gauge/GCSys/%d", m.GCSys),
+				fmt.Sprintf("/update/gauge/HeapAlloc/%d", m.HeapAlloc),
+				fmt.Sprintf("/update/gauge/HeapIdle/%d", m.HeapIdle),
+				fmt.Sprintf("/update/gauge/HeapInuse/%d", m.HeapInuse),
+				fmt.Sprintf("/update/gauge/HeapObjects/%d", m.HeapObjects),
+				fmt.Sprintf("/update/gauge/HeapReleased/%d", m.HeapReleased),
+				fmt.Sprintf("/update/gauge/HeapReleased/%d", m.HeapReleased),
+				fmt.Sprintf("/update/gauge/HeapSys/%d", m.HeapSys),
+				fmt.Sprintf("/update/gauge/LastGC/%d", m.LastGC),
+				fmt.Sprintf("/update/gauge/Lookups/%d", m.Lookups),
+				fmt.Sprintf("/update/gauge/MCacheInuse/%d", m.MCacheInuse),
+				fmt.Sprintf("/update/gauge/MCacheSys/%d", m.MCacheSys),
+				fmt.Sprintf("/update/gauge/MSpanInuse/%d", m.MSpanInuse),
+				fmt.Sprintf("/update/gauge/MSpanSys/%d", m.MSpanSys),
+				fmt.Sprintf("/update/gauge/Mallocs/%d", m.Mallocs),
+				fmt.Sprintf("/update/gauge/NextGC/%d", m.NextGC),
+				fmt.Sprintf("/update/gauge/NumForcedGC/%d", m.NumForcedGC),
+				fmt.Sprintf("/update/gauge/NumGC/%d", m.NumGC),
+				fmt.Sprintf("/update/gauge/OtherSys/%d", m.OtherSys),
+				fmt.Sprintf("/update/gauge/PauseTotalNs/%d", m.PauseTotalNs),
+				fmt.Sprintf("/update/gauge/StackInuse/%d", m.StackInuse),
+				fmt.Sprintf("/update/gauge/StackSys/%d", m.StackSys),
+				fmt.Sprintf("/update/gauge/Sys/%d", m.Sys),
+				fmt.Sprintf("/update/gauge/TotalAlloc/%d", m.TotalAlloc),
+				// Other metrics
+				fmt.Sprintf("/update/counter/PollCount/%d", pollCount),
+				fmt.Sprintf("/update/counter/RandomValue/%d", rand.Int63()),
 			}
-			_ = res.Body.Close()
+
+			for _, path := range paths {
+				path := path
+				go func() {
+					res, err := c.Post(host+path, "text/plain", nil)
+
+					if err != nil {
+						fmt.Printf("Error: %v\n", err)
+						return
+					}
+					_ = res.Body.Close()
+				}()
+			}
 		}
 	}
 }
