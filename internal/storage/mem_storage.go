d49add090fe89f5ef479431f2b3ef844da51dc3c
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-15 22:41:58 +0300

    [Agent] Add RWMutex

--- internal/storage/mem_storage.go
+++ internal/storage/mem_storage.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 const (
@@ -12,6 +13,7 @@ const (
 )
 
 type MemStorage struct {
+	mutex   sync.RWMutex
 	gauge   map[string]float64
 	counter map[string]int64
 }
@@ -19,12 +21,16 @@ type MemStorage struct {
 // TODO use sync.RWMutex https://github.com/varkadov/go-practice-course/pull/5#discussion_r1289184589
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
+		mutex:   sync.RWMutex{},
 		gauge:   make(map[string]float64),
 		counter: make(map[string]int64),
 	}
 }
 
 func (s *MemStorage) Get(metricType, metricName string) (string, error) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
 	if metricType == metricTypeGauge {
 		if v, ok := s.gauge[metricName]; ok {
 			return strconv.FormatFloat(v, 'f', -1, 64), nil
@@ -45,6 +51,9 @@ func (s *MemStorage) Get(metricType, metricName string) (string, error) {
 func (s *MemStorage) GetAll() []string {
 	l := make([]string, 0)
 
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
 	for k, v := range s.gauge {
 		l = append(l, fmt.Sprintf("%s/%s: %s", metricTypeGauge, k, strconv.FormatFloat(v, 'f', -1, 64)))
 	}
@@ -57,6 +66,9 @@ func (s *MemStorage) GetAll() []string {
 }
 
 func (s *MemStorage) Set(metricType, metricName, metricValue string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	if metricType == metricTypeGauge {
 		value, err := strconv.ParseFloat(metricValue, 64)
 		if err != nil {
