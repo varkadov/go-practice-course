db9358eff2bdf1d91ad5429ac4d4795bca3e7a80
Vadim Arkadov <vgarkadov@avito.ru>
2023-10-03 00:21:53 +0300

    [server] Compress responses

--- internal/middlewares/with_gzip.go
+++ internal/middlewares/with_gzip.go
@@ -7,11 +7,19 @@ import (
 	"strings"
 )
 
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (grw *gzipResponseWriter) Write(b []byte) (int, error) {
+	return grw.Writer.Write(b)
+}
+
 func WithGzip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		contentEncoding := r.Header.Get("Content-Encoding")
-
-		if strings.Contains(contentEncoding, "gzip") {
+		// Handle requests
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 			reader, err := gzip.NewReader(r.Body)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("Error creating gzip reader: %v", err), http.StatusBadRequest)
@@ -21,6 +29,14 @@ func WithGzip(next http.Handler) http.Handler {
 
 			r.Body = reader
 			r.Header.Del("Content-Encoding")
+		}
+
+		// Handle responses
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gzw := gzip.NewWriter(w)
+			defer gzw.Close()
+			w = &gzipResponseWriter{Writer: gzw, ResponseWriter: w}
 		}
 
 		next.ServeHTTP(w, r)
