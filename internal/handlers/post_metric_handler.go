9a832aee2105b373835fddc397e686605d2ac6a4
Vadim Arkadov <vgarkadov@avito.ru>
2023-10-08 09:55:03 +0300

    [server] Update old endpoint

--- internal/handlers/post_metric_handler.go
+++ internal/handlers/post_metric_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
@@ -12,7 +13,21 @@ func (h *Handler) PostMetricHandler(w http.ResponseWriter, r *http.Request) {
 	metricName := strings.ToLower(chi.URLParam(r, "metricName"))
 	metricValue := chi.URLParam(r, "metricValue")
 
-	_, err := h.storage.Set(metricType, metricName, metricValue)
+	m, err := h.storage.Set(metricType, metricName, metricValue)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	value := ""
+
+	if m.Value != nil {
+		value = strconv.FormatFloat(*m.Value, 'f', -1, 64)
+	} else if m.Delta != nil {
+		value = strconv.FormatInt(*m.Delta, 10)
+	}
+
+	_, err = w.Write([]byte(value))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
