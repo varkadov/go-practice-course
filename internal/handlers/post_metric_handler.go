d8acca07c6c876a6d71a8f6188a94c6abd46366d
Vadim Arkadov <vgarkadov@avito.ru>
2023-10-08 10:01:56 +0300

    Revert "[server] Update old endpoint"
    This reverts commit 9a832aee2105b373835fddc397e686605d2ac6a4.

--- internal/handlers/post_metric_handler.go
+++ internal/handlers/post_metric_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
@@ -13,21 +12,7 @@ func (h *Handler) PostMetricHandler(w http.ResponseWriter, r *http.Request) {
 	metricName := strings.ToLower(chi.URLParam(r, "metricName"))
 	metricValue := chi.URLParam(r, "metricValue")
 
-	m, err := h.storage.Set(metricType, metricName, metricValue)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	value := ""
-
-	if m.Value != nil {
-		value = strconv.FormatFloat(*m.Value, 'f', -1, 64)
-	} else if m.Delta != nil {
-		value = strconv.FormatInt(*m.Delta, 10)
-	}
-
-	_, err = w.Write([]byte(value))
+	_, err := h.storage.Set(metricType, metricName, metricValue)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
